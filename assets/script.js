c3ecfd5c9c8b9d30522f2f5d28379c299a835976
Thomas CHE <jordanlafond76@gmail.com>
2026-09-01 11:48:52 +0200

    Part 3 v2

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,21 @@ const slides = [
 const leftArrow = document.querySelector(".arrow_left");
 const rightArrow = document.querySelector(".arrow_right");
 
+// Selection du conteneur des points
+const dotsContainer = document.querySelector(".dots");
+
+// Generation d'un point pour chaque slide
+slides.forEach((_, index) => {
+	const dot = document.createElement("div");
+	dot.classList.add("dot");
+
+	if (index === 0) {
+		dot.classList.add("dot_selected");
+	}
+
+	dotsContainer.appendChild(dot);
+});
+
 // Test des clics sur les fleches
 leftArrow.addEventListener("click", () => {
 	console.log("Clic sur la fleche gauche");
